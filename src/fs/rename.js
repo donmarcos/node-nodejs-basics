7b7489e4894ca1ebbfcfb3e15926f11c5ab93529
unknown <marcos.alois.oliva@gmail.com>
2025-05-04 17:37:00 -0700

    init: start Task1 rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { logMsg, getPathData } from '../utils.js';
+
+const dirName = 'files';
+const oldFilename = 'wrongFilename.txt';
+const newFilename = 'properFilename.md';
+const { __dirname } = getPathData(import.meta.url);
+
+const errorMap = {
+    ENOENT: 'FS operation failed',
+    EEXIST: 'FS operation failed',
+};
+
 const rename = async () => {
     // Write your code here 
+    logMsg({ msg: 'Starting work rename.js' });
+
+
+
+
+
+
+    logMsg({ msg: 'Ending work rename.js' });
 };
 
 await rename();
