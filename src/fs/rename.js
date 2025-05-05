df2f36a9bef2460c625e8f287ca83b8496c962ad
unknown <marcos.alois.oliva@gmail.com>
2025-05-04 17:58:34 -0700

    feat: implement rename process and logic

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -3,8 +3,8 @@ import path from 'node:path';
 import { logMsg, getPathData } from '../utils.js';
 
 const dirName = 'files';
-const oldFilename = 'wrongFilename.txt';
-const newFilename = 'properFilename.md';
+const oldFileName = 'wrongFilename.txt';
+const newFileName = 'properFilename.md';
 const { __dirname } = getPathData(import.meta.url);
 
 const errorMap = {
@@ -15,13 +15,44 @@ const errorMap = {
 const rename = async () => {
     // Write your code here 
     logMsg({ msg: 'Starting work rename.js' });
+    const oldPath = path.join(__dirname, dirName, oldFilename);
+    const newPath = path.join(__dirname, dirName, newFilename);
 
+    if (!(await isFileExists(oldPath))) {
+        logMsg(
+            `$(errorMap.EEXIST}. The File ${newFileName} does not exist`,
+            'error'
+        )
+    }
 
+    if ((await isFileExists(newPath))) {
+        logMsg(
+            `$(errorMap.EEXIST}. The File ${newFileName} already exist`,
+            'error'
+        )
+    }
 
-
+    try {
+        await fs.rename(oldPath, newPath);
+        logMsg(`Success: file ${oldFileName} renamed to ${newFileName}`);
+    } catch (error) {
+        const errorMessage = errorMap[error.code] || error.errorMessage;
+        logMsg({
+            msg: errorMessage,
+            cause: error,
+            type: 'error',
+        });
+    }
 
 
     logMsg({ msg: 'Ending work rename.js' });
 };
+
+function isFileExists(filePath) {
+    return fs
+        .access(filePath)
+        .then(() => true)
+        .catch(() => false);
+}
 
 await rename();
