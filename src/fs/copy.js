5a9986c2db1185d14e5dc934971d047ee0fda750
unknown <marcos.alois.oliva@gmail.com>
2025-05-04 17:17:43 -0700

    feat: implement copy process and logic

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -8,6 +8,7 @@ const sourceDirname = 'files';
 const targetDirname = 'files_copy';
 const { __dirname } = getPathData(import.meta.url);
 
+const ERR_FS_OPERATION_FAILED = 'FS operation failed';
 const errorMap = {
     ENOENT: 'FS operation failed',
     EEXIST: 'FS operation failed',
@@ -17,6 +18,28 @@ const errorMap = {
 const copy = async () => {
     // Write your code here 
     logMsg({ msg: 'Starting work copy.js' });
+
+    const sourcePath = path.join(__dirname, sourceDirname);
+    const targetPath = path.join(__dirname, targetPath);
+
+    try {
+        await fs.cp(sourcePath, targetPath, {
+            recursive: true,
+            errorOnExist: true,
+            force: false,
+        });
+        logMsg({
+            msg: `Success: Directory '${sourceDirname}' copied to '${targetDirname}'`
+        });
+    } catch (error) {
+        const errorMessage = errorMap[error.code] || error.errorMessage;
+        logMsg({
+            msg: errorMessage,
+            cause: error,
+            type: 'error',
+        });
+    }
+
 
     logMsg({ msg: 'Ending work copy.js' });
 };
