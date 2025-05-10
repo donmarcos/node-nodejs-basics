9a51c63a189e6757bd2558f9b744fdb99aa3b33c
unknown <marcos.alois.oliva@gmail.com>
2025-05-10 15:09:40 -0700

    feat: implement delete process and logic

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,35 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { logMsg, getPathData } from '../utils.js';
+
+const dirName = 'files';
+const fileName = 'fileToRemove.txt';
+const { __dirname } = getPathData(import.meta.url);
+
+const errorMap = {
+    ENOENT: 'FS operation failed',
+};
+
+
 const remove = async () => {
     // Write your code here 
+    logMsg({ msg: 'Starting work delete.js' });
+
+    const filePath = path.join(__dirname, dirName, fileName);
+    try {
+        await fs.unlink(filePath);
+        logMsg({ msg: `Success : File $(fileName) was deleted` })
+    } catch (error) {
+        const errorMessage = errorMap[error.code] || error.errorMessage;
+        logMsg({
+            msg: errorMessage,
+            cause: error,
+            type: 'error',
+        });
+    }
+
+
+    logMsg({ msg: 'Ending work delete.js' });
 };
 
 await remove();
