a91948307c6c004cd5ad8ce85f6c9158ab403bac
unknown <marcos.alois.oliva@gmail.com>
2025-05-10 16:56:30 -0700

    feat: implement Read process and logic

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,38 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { logMsg, getPathData } from '../utils.js';
+
+const encoding = 'utf8';
+const dirName = 'files';
+const fileName = 'fileToRead.txt';
+const { __dirname } = getPathData(import.meta.url);
+const filePath = path.join(__dirname, dirName, fileName);
+
+const errorMap = {
+    ENOENT: 'FS operation failed',
+};
+
 const read = async () => {
     // Write your code here 
+    logMsg({ msg: 'Starting work Read.js' });
+
+    try {
+        const content = await fs.readFile(filePath, encoding);
+        logMsg({ msg: content, type: 'important' });
+        logMsg({ msg: 'Success: Files content printed' });
+
+    } catch (error) {
+        const errorMessage = errorMap[error.code] || error.errorMessage;
+        logMsg({
+            msg: errorMessage,
+            cause: error,
+            type: 'error',
+        });
+    }
+
+
+    logMsg({ msg: 'Endig work Read.js' });
+
 };
 
 await read();
