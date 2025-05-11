2ed4357f0da825347b874d1be81f54fd81969904
unknown <marcos.alois.oliva@gmail.com>
2025-05-10 17:20:49 -0700

    fix: implement better process to read in the process.env file using Object.entries

--- src/cli/env.js
+++ src/cli/env.js
@@ -8,18 +8,17 @@ const parseEnv = () => {
     // Write your code here 
     logMsg({ msg: 'Starting work Env.js' });
     const envVars = process.env;
-    const rssVars = [];
+    //const rssVars = [];
 
-    for (let key in envVars) {
-        if (key.startsWith('RSS_')) {
-            rssVars.push(`${key}=${envVars[key]}`);
-        }
-    }
+    // Reviewed code for better approach 
+    const rssVars = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`);
 
     if (rssVars === 0) {
         logMsg({ msg: 'No RSS variables found', type: 'important' });
     } else {
-        logMsg({ msg: rssVars.join(os.EOL), type: 'important' });
+        logMsg({ msg: rssVars.join('; '), type: 'important' });
     }
 
     logMsg({ msg: 'Ending work Env.js' });
