bb50ba0e71d685edfd46947a66030d359ff77eee
unknown <marcos.alois.oliva@gmail.com>
2025-05-10 17:11:22 -0700

    feat: implement Env process and logic

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,29 @@
+
+import { logMsg } from '../utils.js';
+import os from 'node:os';
+
+
+
 const parseEnv = () => {
     // Write your code here 
+    logMsg({ msg: 'Starting work Env.js' });
+    const envVars = process.env;
+    const rssVars = [];
+
+    for (let key in envVars) {
+        if (key.startsWith('RSS_')) {
+            rssVars.push(`${key}=${envVars[key]}`);
+        }
+    }
+
+    if (rssVars === 0) {
+        logMsg({ msg: 'No RSS variables found', type: 'important' });
+    } else {
+        logMsg({ msg: rssVars.join(os.EOL), type: 'important' });
+    }
+
+    logMsg({ msg: 'Ending work Env.js' });
+
 };
 
 parseEnv();
