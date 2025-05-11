a170d02c16111216f593c7c9d5e8e0ecbe9cf8ab
unknown <marcos.alois.oliva@gmail.com>
2025-05-10 17:29:27 -0700

    feat: implement initial Args  process and logic

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,24 @@
+import { logMsg } from '../utils.js';
+import os from 'node:os';
+
+
 const parseArgs = () => {
     // Write your code here 
+    logMsg({ msg: 'Starting work Args.js' });
+
+    const args = process.argv.slice(2);
+
+    if (args.length === 0) {
+        logMsg({ msg: 'No arguments found', type: 'important' });
+    }
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].slice(2);
+        const value = args[i + 1];
+        logMsg({ msg: `${propName} is ${value}`, type: 'important' });
+    }
+
+    logMsg({ msg: 'Ending work Args.js' });
 };
 
 parseArgs();
